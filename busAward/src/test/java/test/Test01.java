f6fdbadc186639e0e8b76ab6e4d5787cc01f1c0a
margindream <54886406+margindream@users.noreply.github.com>
2019-10-31 20:51:35 +0800

    10

--- busAward/src/test/java/test/Test01.java
+++ busAward/src/test/java/test/Test01.java
@@ -2,6 +2,6 @@ package test;
 
 public class Test01 {
 	public static void main(String[] args) {
-		System.out.println("111111111111111111111111111111111dwerwerfawetrsergse");
+		System.out.println("1111111111111");
 	}
 }
